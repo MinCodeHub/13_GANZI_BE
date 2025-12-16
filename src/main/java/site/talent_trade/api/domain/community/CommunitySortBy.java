c85a637ee04b5f872e81d2e2e85e141f64ec0b74
me_nyong <gjalsdud1030@naver.com>
2025-12-16 15:44:40 +0900

    refactor: SortBy Enum 도메인에 맞춰 분리

--- src/main/java/site/talent_trade/api/domain/community/CommunitySortBy.java
+++ src/main/java/site/talent_trade/api/domain/community/CommunitySortBy.java
@@ -1,11 +1,9 @@
 package site.talent_trade.api.domain.community;
 
-public enum SortBy {
+public enum CommunitySortBy {
     //커뮤니티 필터링
     LATEST, //최신글 순
     HTI_COUNT, //조회 순
     COMMENT_COUNT, //댓글 순
     RECENT_MEMBER, //최근 가입자 순
-    REVIEW, //리뷰 개수 순
-    SCORE //평점 높은 순
 }
---
+++ src/main/java/site/talent_trade/api/domain/member/MemberSortBy.java
@@ -0,0 +1,6 @@
+package site.talent_trade.api.domain.member;
+
+public enum MemberSortBy {
+    REVIEW, //리뷰 개수 순
+    SCORE //평점 높은 순
+}
